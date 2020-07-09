5004504591af12e5e12da71c6dffef3c7f9e7f58
A-Damian11 <alandamian11.fr@gmail.com>
2020-07-09 10:11:16 -0500

    Programa 2 Finalizado

--- prog2.py
+++ prog2.py
@@ -2,8 +2,15 @@
 #calcular la suma desde el numero 1 hasta el numero 2.
 #imprimir el resultado de la suma
 
-print ("Programa que muestra la suma de 2 numeros enteros")
-num1 = int(input("Ingrese el primer numero a sumar:\n "))
-num2 = int(input("Ingrese el segundo numero a sumar:\n "))
-suma = (num1+num2)
-print(f'El resultado de su suma es:\n {suma}')
+suma = 0
+print ("Programa que muestra la suma de el numero1 hasta el 2")
+num1 = int(input("Ingrese el primer numero:\n "))
+num2 = int(input("Ingrese el segundo numero:\n "))
+num2 = num2 + 1
+for num in range (num1,num2):
+    suma += num
+
+print (f'El resultado es:\n {suma}')
+
+
+
