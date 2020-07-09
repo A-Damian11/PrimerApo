af43bc0a6f6567c08de65a6b839db512b6d3f76e
A-Damian11 <alandamian11.fr@gmail.com>
2020-07-09 10:01:31 -0500

    Programa 2 finalizado

---
+++ prog2.py
@@ -0,0 +1,9 @@
+#Escribir un programa que pida al usuario 2 numeros enteros y
+#calcular la suma desde el numero 1 hasta el numero 2.
+#imprimir el resultado de la suma
+
+print ("Programa que muestra la suma de 2 numeros enteros")
+num1 = int(input("Ingrese el primer numero a sumar:\n "))
+num2 = int(input("Ingrese el segundo numero a sumar:\n "))
+suma = (num1+num2)
+print(f'El resultado de su suma es:\n {suma}')
